a52e21cd5978dd3b61b8c655985ce2e9b09794fc
franpax95 <fcojnavarrowork@gmail.com>
2024-10-17 20:51:49 +0200

    feat(page): add sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
+import './styles.scss';
+import { Header } from './components/Header';
+import { About } from './components/About';
+import { HighlightedText } from './components/HighlightedText';
+import { Services } from './components/Services';
+import { WhyWorkWithMe } from './components/WhyWorkWithMe';
+
 export default function Home() {
-  return <div>This is the home page</div>;
+  return (
+    <div className="home">
+      <Header />
+
+      <About />
+
+      <div className="home__highlight">
+        <HighlightedText>
+          let&apos;s turn your brand into the next social media success!
+        </HighlightedText>
+      </div>
+
+      <Services />
+
+      <WhyWorkWithMe />
+    </div>
+  );
 }
