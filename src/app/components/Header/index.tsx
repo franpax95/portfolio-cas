492a571ebaf00d3b0c8c9007839b95bbff996cb9
franpax95 <fcojnavarrowork@gmail.com>
2024-10-17 21:08:09 +0200

    feat(header): add bg

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,8 +1,13 @@
+import Image from 'next/image';
 import './styles.scss';
 
 export const Header = () => {
   return (
     <header className="header">
+      <div className="header__bg">
+        <Image src="/header.png" alt="Cas' portfolio header" fill />
+      </div>
+
       <span className="header__title">Social Media isn&apos;t just a tool;</span>
       <span className="header__subtitle">it&apos;s a game-changer.</span>
     </header>
