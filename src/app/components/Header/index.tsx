2bcfe40803bab100bdc5963e71aa8dc703d80a12
franpax95 <fcojnavarrowork@gmail.com>
2024-10-17 20:50:07 +0200

    feat(components): header comp

---
+++ src/app/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import './styles.scss';
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <span className="header__title">Social Media isn&apos;t just a tool;</span>
+      <span className="header__subtitle">it&apos;s a game-changer.</span>
+    </header>
+  );
+};
