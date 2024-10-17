d38bb6fee1372a4228d5a59d5551bc2de62a1443
franpax95 <fcojnavarrowork@gmail.com>
2024-10-17 20:50:39 +0200

    feat(components): navbar

---
+++ src/app/components/Navbar/index.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+import './styles.scss';
+import Image from 'next/image';
+
+export const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navbar__img-wrapper">
+        <Image
+          className="navbar__img"
+          src="/test-logo.svg"
+          alt="Cassandra B Neumann Portfolio"
+          width={150}
+          height={60}
+        />
+      </div>
+
+      <div className="navbar__links">
+        <Link className="navbar__link" href="/">
+          About
+        </Link>
+        <Link className="navbar__link" href="/">
+          Portfolio
+        </Link>
+        <Link className="navbar__link" href="/">
+          Services
+        </Link>
+        <Link className="navbar__link" href="/">
+          Packages
+        </Link>
+        <Link className="navbar__link" href="/">
+          Contact
+        </Link>
+      </div>
+    </div>
+  );
+};
