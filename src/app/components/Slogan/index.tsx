480dee3d6270d3b8caa506fada5a90d5fa7d7746
franpax95 <fcojnavarrowork@gmail.com>
2024-10-20 18:28:56 +0200

    feat(slogan): add animations

--- src/app/components/Slogan/index.tsx
+++ src/app/components/Slogan/index.tsx
@@ -1,16 +1,55 @@
+'use client';
+
 import Image from 'next/image';
 import './styles.scss';
+import useIntersectionObserver from '@/app/hooks/useIntersectionObserver';
+import { useEffect, useState } from 'react';
 
 export const Slogan = () => {
-  return (
-    <section className="slogan">
-      <div className="slogan__content">
-        <p className="slogan__text">
-          {`Immerse\nyourself in a\nvisual journey\nwhere\n`}
-          <span className="slogan__purple">{`creativity\nmeets impact.`}</span>
-        </p>
+  const [threshold, setThreshold] = useState<number>(0.85);
+  const [animate, set] = useState<boolean>(false);
+  const [sectionRef, isVisible] = useIntersectionObserver<HTMLDivElement>({
+    root: null,
+    rootMargin: '0px',
+    threshold
+  });
 
-        <div className="slogan__gallery">
+  useEffect(() => {
+    const updateThreshold = () => {
+      const screenWidth = window.innerWidth;
+      if (screenWidth < 600) {
+        setThreshold(0.5);
+      } else {
+        setThreshold(0.85);
+      }
+    };
+
+    window.addEventListener('resize', updateThreshold);
+
+    updateThreshold();
+
+    return () => {
+      window.removeEventListener('resize', updateThreshold);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isVisible) {
+      set(true);
+    }
+  }, [isVisible]);
+
+  return (
+    <section className="slogan" ref={sectionRef}>
+      <div className="slogan__content">
+        <div className={`slogan__text-wrapper ${animate && 'slogan__text-wrapper--animated'}`}>
+          <p className={`slogan__text ${animate && 'slogan__text--animatedfff'}`}>
+            {`Immerse\nyourself in a\nvisual journey\nwhere\n`}
+            <span className="slogan__purple">{`creativity\nmeets impact.`}</span>
+          </p>
+        </div>
+
+        <div className={`slogan__gallery ${animate && 'slogan__gallery--animated'}`}>
           <div className="slogan__img-wrapper slogan__img-wrapper--first">
             <Image src="/slogan/1.png" alt="Slogan 1" width={240} height={300} />
           </div>
