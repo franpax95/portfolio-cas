6e8b89854a7b46cead089e3680859f8a4a3429b3
franpax95 <fcojnavarrowork@gmail.com>
2024-10-20 15:39:32 +0200

    feat(branding): add brand imgs

--- src/app/components/Branding/data.ts
+++ src/app/components/Branding/data.ts
@@ -5,4 +5,41 @@ export interface Brand {
   height: number;
 }
 
-export const brands: Array<Brand> = [];
+export const brands: Array<Brand> = [
+  {
+    src: '/branding/ntf.svg',
+    alt: 'New Talent Festival',
+    width: 308,
+    height: 386
+  },
+  {
+    src: '/branding/groupon.svg',
+    alt: 'Groupon',
+    width: 469,
+    height: 101
+  },
+  {
+    src: '/branding/right_whale.svg',
+    alt: 'Right Whale',
+    width: 468,
+    height: 70
+  },
+  {
+    src: '/branding/avant.svg',
+    alt: 'Avant',
+    width: 308,
+    height: 386
+  },
+  {
+    src: '/branding/maps.svg',
+    alt: 'Awesome Maps',
+    width: 450,
+    height: 139
+  },
+  {
+    src: '/branding/les_enfants.svg',
+    alt: 'Les Enfants',
+    width: 458,
+    height: 237
+  }
+];
