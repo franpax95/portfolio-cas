e3d284353394b966cd05bae6b58957d125146956
franpax95 <fcojnavarrowork@gmail.com>
2024-10-20 17:45:52 +0200

    feat(testimonials): add quotes animation

--- src/app/components/Testimonials/index.tsx
+++ src/app/components/Testimonials/index.tsx
@@ -1,13 +1,32 @@
+'use client';
+
+import useIntersectionObserver from '@/app/hooks/useIntersectionObserver';
 import './styles.scss';
+import { useEffect, useState } from 'react';
 import { Testimonial, testimonials } from './data';
 import { Button } from '../Button';
 import Image from 'next/image';
 
 export const Testimonials = () => {
+  const [animate, set] = useState<boolean>(false);
+  const [sectionRef, isVisible] = useIntersectionObserver<HTMLDivElement>({
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.85
+  });
+
+  useEffect(() => {
+    if (isVisible) {
+      set(true);
+    }
+  }, [isVisible]);
+
   return (
-    <section className="testimonials">
+    <section className="testimonials" ref={sectionRef}>
       <div className="testimonials__content">
-        <div className="testimonials__quotes testimonials__quotes--initial">
+        <div
+          className={`testimonials__quotes testimonials__quotes--initial ${animate && 'testimonials__quotes--animated'}`}
+        >
           <Image src="/testimonials/quote_1.svg" alt="quote 1" width={80} height={80} />
         </div>
 
@@ -24,7 +43,9 @@ export const Testimonials = () => {
 
         <Button light>To see more testimonials, click here</Button>
 
-        <div className="testimonials__quotes testimonials__quotes--end">
+        <div
+          className={`testimonials__quotes testimonials__quotes--end ${animate && 'testimonials__quotes--animated'}`}
+        >
           <Image src="/testimonials/quote_2.svg" alt="quote 2" width={80} height={80} />
         </div>
       </div>
