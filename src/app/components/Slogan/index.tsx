c6cf6d183bc9e73e46f16fdf1c61b03e516b415f
franpax95 <fcojnavarrowork@gmail.com>
2024-10-20 16:19:07 +0200

    style(slogan): add box shadow effect

--- src/app/components/Slogan/index.tsx
+++ src/app/components/Slogan/index.tsx
@@ -3,29 +3,31 @@ import './styles.scss';
 
 export const Slogan = () => {
   return (
-    <div className="slogan">
-      <p className="slogan__text">
-        {`Immerse\nyourself in a\nvisual journey\nwhere\n`}
-        <span className="slogan__purple">{`creativity\nmeets impact.`}</span>
-      </p>
+    <section className="slogan">
+      <div className="slogan__content">
+        <p className="slogan__text">
+          {`Immerse\nyourself in a\nvisual journey\nwhere\n`}
+          <span className="slogan__purple">{`creativity\nmeets impact.`}</span>
+        </p>
 
-      <div className="slogan__gallery">
-        <div className="slogan__img-wrapper slogan__img-wrapper--first">
-          <Image src="/slogan/1.png" alt="Slogan 1" width={240} height={300} />
-        </div>
+        <div className="slogan__gallery">
+          <div className="slogan__img-wrapper slogan__img-wrapper--first">
+            <Image src="/slogan/1.png" alt="Slogan 1" width={240} height={300} />
+          </div>
 
-        <div className="slogan__img-wrapper slogan__img-wrapper--second">
-          <Image src="/slogan/2.png" alt="Slogan 2" width={240} height={300} />
-        </div>
+          <div className="slogan__img-wrapper slogan__img-wrapper--second">
+            <Image src="/slogan/2.png" alt="Slogan 2" width={240} height={300} />
+          </div>
 
-        <div className="slogan__img-wrapper slogan__img-wrapper--third">
-          <Image src="/slogan/3.png" alt="Slogan 3" width={240} height={300} />
-        </div>
+          <div className="slogan__img-wrapper slogan__img-wrapper--third">
+            <Image src="/slogan/3.png" alt="Slogan 3" width={240} height={300} />
+          </div>
 
-        <div className="slogan__img-wrapper slogan__img-wrapper--fourth">
-          <Image src="/slogan/4.png" alt="Slogan 4" width={240} height={300} />
+          <div className="slogan__img-wrapper slogan__img-wrapper--fourth">
+            <Image src="/slogan/4.png" alt="Slogan 4" width={240} height={300} />
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
