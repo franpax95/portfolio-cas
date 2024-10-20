2a259a47c13becd1ecf0e595a7e89658c5e7b462
franpax95 <fcojnavarrowork@gmail.com>
2024-10-20 15:39:50 +0200

    feat(slogan): add slogan imgs

--- src/app/components/Slogan/index.tsx
+++ src/app/components/Slogan/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import './styles.scss';
 
 export const Slogan = () => {
@@ -9,10 +10,21 @@ export const Slogan = () => {
       </p>
 
       <div className="slogan__gallery">
-        <div className="slogan__img-wrapper slogan__img-wrapper--first"></div>
-        <div className="slogan__img-wrapper slogan__img-wrapper--second"></div>
-        <div className="slogan__img-wrapper slogan__img-wrapper--third"></div>
-        <div className="slogan__img-wrapper slogan__img-wrapper--fourth"></div>
+        <div className="slogan__img-wrapper slogan__img-wrapper--first">
+          <Image src="/slogan/1.png" alt="Slogan 1" width={240} height={300} />
+        </div>
+
+        <div className="slogan__img-wrapper slogan__img-wrapper--second">
+          <Image src="/slogan/2.png" alt="Slogan 2" width={240} height={300} />
+        </div>
+
+        <div className="slogan__img-wrapper slogan__img-wrapper--third">
+          <Image src="/slogan/3.png" alt="Slogan 3" width={240} height={300} />
+        </div>
+
+        <div className="slogan__img-wrapper slogan__img-wrapper--fourth">
+          <Image src="/slogan/4.png" alt="Slogan 4" width={240} height={300} />
+        </div>
       </div>
     </div>
   );
