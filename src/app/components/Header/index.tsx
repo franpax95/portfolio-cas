8113aa0b1ef874d1c355e7b6c01803c97eb6a610
franpax95 <fcojnavarrowork@gmail.com>
2024-10-20 18:43:31 +0200

    feat(assets): add assets

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
   return (
     <header className="header">
       <div className="header__bg">
-        <Image src="/header.png" alt="Cas' portfolio header" fill />
+        <Image src="/header/header.png" alt="Cas' portfolio header" fill />
       </div>
 
       <span className="header__title">Social Media isn&apos;t just a tool;</span>
